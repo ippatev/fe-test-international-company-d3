194555e427b14d2e7bfd106ca1d962129e7884d9
Aleksandr Ippatev <alexanderippatiev24@gmail.com>
2025-03-10 10:57:04 +0900

    feat: update app root template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, NO_ERRORS_SCHEMA, OnInit } from "@angular/core";
+import { DataService } from "@services/data.service";
+import { Task } from "@models/task.model";
+import { CommonModule } from "@angular/common";
+import { TimelineComponent } from "@components/timeline/timeline.component";
+import { WorkerListComponent } from "@components/worker-list/worker-list.component";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: "app-root",
+  standalone: true,
+  imports: [CommonModule, FormsModule, TimelineComponent, WorkerListComponent],
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'fe-test-international-company-d3';
+  tasks;
+  selectedTask: Task | null = null;
+
+  constructor(public dataService: DataService) {
+    this.tasks = this.dataService.getTasks();
+  }
+
+  onSelectTask(task: Task): void {
+    this.selectedTask = task;
+  }
 }
